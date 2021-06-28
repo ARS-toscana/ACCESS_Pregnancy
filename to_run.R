a4240d2b3c6d17e7bdc7832f0cbc4a332644450d
GiorgioLimoncella <giorgio.limoncella@ars.toscana.it>
2021-06-28 10:32:32 +0200

    update

--- to_run.R
+++ to_run.R
@@ -49,8 +49,8 @@ source(paste0(thisdir,"/p_steps/step_03_05b_T2.2_internal_consistency_for_concep
 source(paste0(thisdir,"/p_steps/step_03_05c_T2.2_internal_consistency_for_EUROCAT.R")) 
 source(paste0(thisdir,"/p_steps/step_03_05d_T2.2_internal_consistency_for_itemsets.R")) 
 
-source(paste0(thisdir,"/p_steps/step_03_06_T2.3_merge_stream_of_same_person.R")) 
-source(paste0(thisdir,"/p_steps/step_03_07_T2.2_process_pregnancies_excluded.R"))
+source(paste0(thisdir,"/p_steps/step_03_06_1_T2.2_process_pregnancies_excluded.R")) 
+source(paste0(thisdir,"/p_steps/step_03_06_2_T2.3_merge_stream_of_same_person.R")) 
 
 # 04 CREATE PREGNANCIES outcomes
 source(paste0(thisdir,"/p_steps/step_04_01_T2_create_pregnancy_outcomes.R")) 
